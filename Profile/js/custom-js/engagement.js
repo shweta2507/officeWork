c8c919cb30b87aa2a543a7747f4113f7d197f812
unknown <shweta.vashisth1@gmail.com>
2021-04-01 19:02:26 +0530

    working

--- Profile/js/custom-js/engagement.js
+++ Profile/js/custom-js/engagement.js
@@ -16,7 +16,7 @@
 
 
     
-    $('#add-conf-popup #Confirmation-button .btn-blue').click(function(ele){
+    $('#add-conf-popup #Confirmation-button .btn-blue, #add-hyg-popup #Hygiene-button .btn-blue, #add-rem-popup #reminder-button .btn-blue, #add-conf-popup2 #Confirmation-button4 .btn-blue, #add-hyg-popup4 #Hygiene-button4 .btn-blue, #add-rem-popup5 #reminder-button5 .btn-blue').click(function(ele){
         var $this = $(ele.currentTarget);
         setTimeout(function(){
         if($this.closest('.upload-img-wrapper').find('.show-error').length === 0){
@@ -32,7 +32,7 @@
 
     jQuery(document).ready(function() {
         (function() {
-            var showChar = 200;
+            var showChar = 86;
             var ellipsestext = "...";
             jQuery(".inner-content > .inner-content-text").each(function() {
             var content = jQuery(this).html();
@@ -47,11 +47,24 @@
                 '&nbsp;&nbsp;<a href="" class="moreless more">more</a></span></span></div><div class="truncate-text" style="display:none">' +
                 h +
                 '<a href="" class="moreless less">Less</a></span></div>';
-
                 jQuery(this).html(html);
             }
             });
+            jQuery(".moreless").click(function() {
+                var thisEl = jQuery(this);
+                var cT = thisEl.closest(".truncate-text");
+                var tX = ".truncate-text";
+                if (thisEl.hasClass("less")) {
+                    cT.prev(tX).toggle();
+                    cT.slideToggle();
+                } else {
+                    cT.toggle();
+                    cT.next(tX).fadeToggle();
+                }
+                return false;
+            });
         })();
     });
+    
 
     
